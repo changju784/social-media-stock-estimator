5b8cccdc9a14904f5a97b90e53fc79a674360a43
jungin <changju@bu.edu>
2025-10-11 19:28:43 -0500

    fix config

--- src/crawler/config.py
+++ src/crawler/config.py
@@ -5,4 +5,4 @@ AFTER_DAYS = 90         # crawl posts from past 3 months
 POST_LIMIT = 100         # number of posts per (subreddit, company)
 COMMENT_LIMIT = 20       # number of comments per post
 SLEEP_SEC = 1            # throttle to avoid rate-limit
-SAVE_DIR = "reddit_crawler/data/raw/"
+SAVE_DIR = "data/raw/"
